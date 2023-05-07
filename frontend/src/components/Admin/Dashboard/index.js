b7f89508f5200a3862d2e0b8f6f83f439a5cc3cb
linhcm981a <linhcm981a@gmail.com>
2023-05-07 15:49:58 +0700

    remove frontend and update file package.json

--- frontend/src/components/Admin/Dashboard/index.js
+++
@@ -1,9 +0,0 @@
-import styles from './Dashboard.module.scss';
-import classNames from 'classnames/bind';
-const cx = classNames.bind(styles);
-
-function Dashboard() {
-    return <div className={cx('wrapper')}>Welcome to the siteo3 for admin</div>;
-}
-
-export default Dashboard;
